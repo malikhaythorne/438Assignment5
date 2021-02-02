32c4ac9f8f13f182c1371a762a08935b4e230bbf
malikhaythorne <43774147+malikhaythorne@users.noreply.github.com>
2021-02-01 23:11:14 -0800

    Update textinput.js

--- textinput.js
+++ textinput.js
@@ -1,12 +1,17 @@
-
- import {useState} from 'react'
+import {useState} from 'react'
  
  function TextInput(props) {
    const [text, setText] = useState('')
  
    function sendMessage(){
+     if(text==='') return // skip the function
      props.send(text)
      setText('')
+   }
+   function keyPressed(e){
+     if(e.key==='Enter') {
+       sendMessage()
+     }
    }
  
    return <footer className="text-input">
@@ -14,8 +19,9 @@
        placeholder="Write your message"
        value={text}
        onChange={e=> setText(e.target.value)}
+       onKeyPress={keyPressed}
      />
-     <button onClick={sendMessage}>
+     <button onClick={sendMessage} disabled={!text}>
        SEND
      </button>
    </footer>
