0099d9e8bd6d7bb9232f92a3a711ff01524e7228
malikhaythorne <43774147+malikhaythorne@users.noreply.github.com>
2021-02-25 13:04:32 -0800

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './index.css';
+import ASDF from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+      <ASDF />
+    </React.StrictMode>,
+    document.getElementbyId('root')
+);
