636b000b73e8da4941667d7fb738972ffe01a9ac
malikhaythorne <43774147+malikhaythorne@users.noreply.github.com>
2021-02-25 13:59:20 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,46 @@
 
- import { useState } from 'react';
- import './App.css';
- import TextInput from './TextInput';
+import { useState  from 'react';
+import './App.css'; import TextInput from './TextInput';
+import Message from './Message'
+import NamePicker from './NamePicker'
+import {db, useDB} from './db'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+export default function Wrap() {
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/:room" component={App} />
+    </Switch>
+  </BrowserRouter>
+}
+ function App(props) {
+  const room = props.match.params.room || 'home'
+
+  const messages = useDB(room)
+  const [username,setUsername] = useState(
+    localStorage.getItem('username') || ''
+  )
+
+  // console.log(messages)
+  return <div className="App">
  
- function App() {
-   const [messages,setMessages] = useState([{text:'hello'}])
-   return <div className="App">
-  
-     <header className="header">
-       <div className="logo" />
-       CHATTER
-     </header>
- 
-     <main className="message">
-       {messages.map((m,i)=> {
-         return <Message key={i} {...m} />
-     }}}
-     </main>
- 
-     <TextInput 
-       send={(t)=> setMessages([{text:t}])}
-     />
- 
-   </div>
- }
- 
- export default App;
+    <header className="header">
+      <div className="logo" />
+      CHATTER
+      <NamePicker saveName={setUsername} />
+    </header>
+
+    <main className="messages">
+      {messages.map((msg,i)=> {
+        const isMe = msg.name===username
+        return <Message key={i} {...msg} isMe={isMe} />
+      })}
+    </main>
+
+    <TextInput 
+      send={(t)=> db.send({text:t, name:username, date:new Date(), room})}
+    />
+
+  </div>
+}
